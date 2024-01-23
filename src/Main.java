c74a333d36becfc0246730dcedf07ffd550316e9
BrenoMoura00 <brenoluisadv@gmail.com>
2024-01-23 18:18:44 -0300

    Calculadora Pronta.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Para começar a calculadora escolha a opção a baixo");
+        System.out.println("==================");
+        System.out.println("\"escola uma opção\n" +
+                "\"(1)Soma \n" +
+                "\"(2)Multiplicação\n" +
+                "\"(3)Divisão\n" +
+                "\"(4)Subtração\n");
+        int opcao = scanner.nextInt();
+        if (opcao > 4) {
+            System.out.println("Opção inválida");
+        }
+        System.out.println("Digite o primeiro número:");
+        int numero1 = scanner.nextInt();
+        System.out.println("Digite o segundo número:");
+        int numero2 = scanner.nextInt();
+
+        int resultado = 0;
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Você escolheu a opção Soma ");
+                resultado = numero1 + numero2;
+                break;
+            case 2:
+                System.out.println("Você escolheu a opção Multiplicação ");
+                resultado = numero1 * numero2;
+                break;
+            case 3:
+                System.out.println("Você escolheu a opção Divisão ");
+                resultado = numero1 / numero2;
+                break;
+            case 4:
+                System.out.println("Você escolhe a opção Subtração");
+                resultado = numero1 - numero2;
+                break;
+        }
+        System.out.println("O resultado é :" + resultado);
     }
 }
